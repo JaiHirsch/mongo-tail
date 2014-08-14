0c468ed6212b47e1e676263284e05869304ddfe4
Jai Hirsch <jai.hirsch@gmail.com>
2014-08-14 13:28:34 -0500

    initial RabbitTailType created

--- mongo-tail/src/main/java/org/mongo/runner/OplogTail.java
+++ mongo-tail/src/main/java/org/mongo/runner/OplogTail.java
@@ -3,60 +3,61 @@ package org.mongo.runner;
 import java.util.Map.Entry;
 
 import org.bson.types.BSONTimestamp;
+import org.mongo.tail.TailType;
 
 import com.mongodb.*;
 
 public class OplogTail implements Runnable {
 
-   private MongoClient client = null;
-   private BSONTimestamp lastTimeStamp = null;
-   private DBCollection shardTimeCollection = null;
+	private MongoClient client = null;
+	private BSONTimestamp lastTimeStamp = null;
+	private DBCollection shardTimeCollection = null;
+	private final TailType tailer;
 
-   public OplogTail(Entry<String, MongoClient> client, DB timeDB) {
-      this.client = client.getValue();
-      shardTimeCollection = timeDB.getCollection(client.getKey());
-      DBObject findOne = shardTimeCollection.findOne();
-      if (findOne != null) {
-         lastTimeStamp = (BSONTimestamp) findOne.get("ts");
-      }
-   }
+	public OplogTail(Entry<String, MongoClient> client, DB timeDB,
+			TailType tailer) {
+		this.tailer = tailer;
+		this.client = client.getValue();
+		shardTimeCollection = timeDB.getCollection(client.getKey());
+		DBObject findOne = shardTimeCollection.findOne();
+		if (findOne != null) {
+			lastTimeStamp = (BSONTimestamp) findOne.get("ts");
+		}
+	}
 
-   @Override
-   public void run() {
-      DBCollection fromCollection = client.getDB("local").getCollection("oplog.rs");
-      DBObject timeQuery = getTimeQuery();
-      System.out.println("Start timestamp: " + timeQuery);
-      DBCursor opCursor = fromCollection.find(timeQuery).sort(new BasicDBObject("$natural", 1))
-                                        .addOption(Bytes.QUERYOPTION_TAILABLE).addOption(Bytes.QUERYOPTION_AWAITDATA)
-                                        .addOption(Bytes.QUERYOPTION_NOTIMEOUT);
-      try {
-         while (true) {
-            if (!opCursor.hasNext()) {
-               continue;
-            }
-            else {
-               DBObject nextOp = opCursor.next();
-               lastTimeStamp = ((BSONTimestamp) nextOp.get("ts"));
-               shardTimeCollection.update(new BasicDBObject(),
-                                          new BasicDBObject("$set", new BasicDBObject("ts", lastTimeStamp)), true,
-                                          true, WriteConcern.SAFE);
-               switch ((String) nextOp.get("op")) {
-                  case "u":
-                     if ("repl.time".equals((String) nextOp.get("ns"))) continue;
-                  default:
-                     System.out.println(nextOp);
-               }
-            }
-         }
-      }
-      finally {
-         System.out.println("good bye");
-      }
-   }
+	@Override
+	public void run() {
+		DBCollection fromCollection = client.getDB("local").getCollection(
+				"oplog.rs");
+		DBObject timeQuery = getTimeQuery();
+		System.out.println("Start timestamp: " + timeQuery);
+		DBCursor opCursor = fromCollection.find(timeQuery)
+				.sort(new BasicDBObject("$natural", 1))
+				.addOption(Bytes.QUERYOPTION_TAILABLE)
+				.addOption(Bytes.QUERYOPTION_AWAITDATA)
+				.addOption(Bytes.QUERYOPTION_NOTIMEOUT);
+		try {
+			while (true) {
+				if (!opCursor.hasNext()) {
+					continue;
+				} else {
+					DBObject nextOp = opCursor.next();
+					lastTimeStamp = ((BSONTimestamp) nextOp.get("ts"));
+					shardTimeCollection.update(new BasicDBObject(),
+							new BasicDBObject("$set", new BasicDBObject("ts",
+									lastTimeStamp)), true, true,
+							WriteConcern.SAFE);
+					tailer.tailOp(nextOp);
+				}
+			}
+		} finally {
+			System.out.println("good bye");
+		}
+	}
 
-   private DBObject getTimeQuery() {
-      return lastTimeStamp == null ? new BasicDBObject() : new BasicDBObject("ts", new BasicDBObject("$gt",
-                                                                                                     lastTimeStamp));
-   }
+	private DBObject getTimeQuery() {
+		return lastTimeStamp == null ? new BasicDBObject() : new BasicDBObject(
+				"ts", new BasicDBObject("$gt", lastTimeStamp));
+	}
 
 }
--- mongo-tail/src/main/java/org/mongo/runner/ShardedReplicaTailer.java
+++ mongo-tail/src/main/java/org/mongo/runner/ShardedReplicaTailer.java
@@ -7,6 +7,8 @@ import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.mongo.tail.TailType;
+import org.mongo.tail.TailTypeInjector;
 import org.mongo.util.PropsLoader;
 import org.mongo.util.ShardSetFinder;
 
@@ -15,88 +17,104 @@ import com.mongodb.MongoClient;
 
 public class ShardedReplicaTailer {
 
-   private static MongoClient hostMongoS = null;
-   private static MongoClient timeClient;
-   private static Map<String, MongoClient> shardSetClients;
-   private static DB timeDB;
+	private static MongoClient hostMongoS = null;
+	private static MongoClient timeClient;
+	private static Map<String, MongoClient> shardSetClients;
+	private static DB timeDB;
 
-   public static void main(String[] args) throws UnknownHostException {
+	public static void main(String[] args) throws UnknownHostException {
+		String tailType = "";
+		if (args.length > 0) {
+			tailType = args[0];
+		}
+		try {
+			addShutdownHookToMainThread();
+			establishMongoDBConnections();
+			runTailingThreads(tailType);
+			while (true)
+				;
+		} finally {
+			closeMongoConnections();
+		}
 
-      try {
-         addShutdownHookToMainThread();
-         establishMongoDBConnections();
-         runTailingThreads();
-         while (true)
-            ;
-      }
-      finally {
-         closeMongoConnections();
-      }
+	}
 
-   }
+	private static void establishMongoDBConnections()
+			throws UnknownHostException {
+		Properties mongoConnectionProperties = loadProperties();
+		hostMongoS = new MongoClient(
+				mongoConnectionProperties.getProperty("mongosHostInfo"));
+		timeClient = new MongoClient(
+				mongoConnectionProperties
+						.getProperty(("mongoReplTimeHostInfo")));
+		timeDB = timeClient.getDB("time_d");
+		shardSetClients = new ShardSetFinder().findShardSets(hostMongoS);
+	}
 
-   private static void establishMongoDBConnections() throws UnknownHostException {
-      Properties mongoConnectionProperties = loadProperties();
-      hostMongoS = new MongoClient(mongoConnectionProperties.getProperty("mongosHostInfo"));
-      timeClient = new MongoClient(mongoConnectionProperties.getProperty(("mongoReplTimeHostInfo")));
-      timeDB = timeClient.getDB("time_d");
-      shardSetClients = new ShardSetFinder().findShardSets(hostMongoS);
-   }
+	private static void runTailingThreads(String tailType) {
+		ExecutorService executor = Executors.newFixedThreadPool(shardSetClients
+				.size());
+		for (Entry<String, MongoClient> client : shardSetClients.entrySet()) {
+			Runnable worker = new OplogTail(client, timeDB, getOpType(tailType));
+			executor.execute(worker);
+		}
+		executor.shutdown();
+	}
 
-   private static void runTailingThreads() {
-      ExecutorService executor = Executors.newFixedThreadPool(shardSetClients.size());
-      for (Entry<String, MongoClient> client : shardSetClients.entrySet()) {
-         Runnable worker = new OplogTail(client, timeDB);
-         executor.execute(worker);
-      }
-      executor.shutdown();
-   }
+	private static TailType getOpType(String tailType) {
+		return new TailTypeInjector().getTailTypeFromArgs(tailType);
+	}
 
-   private static void closeMongoConnections() {
-      if (hostMongoS != null) hostMongoS.close();
-      if (timeClient != null) timeClient.close();
-      for (MongoClient repClient : shardSetClients.values()) {
-         repClient.close();
-      }
-   }
+	private static void closeMongoConnections() {
+		if (hostMongoS != null)
+			hostMongoS.close();
+		if (timeClient != null)
+			timeClient.close();
+		for (MongoClient repClient : shardSetClients.values()) {
+			repClient.close();
+		}
+	}
 
-   private static Properties loadProperties() {
-      PropsLoader propsLoader = new PropsLoader();
-      return propsLoader.loadMongoProperties();
-   }
+	private static Properties loadProperties() {
+		PropsLoader propsLoader = new PropsLoader();
+		return propsLoader.loadMongoProperties();
+	}
 
-   public static void addShutdownHookToMainThread() {
-      final Thread mainThread = Thread.currentThread();
-      Runtime.getRuntime().addShutdownHook(new Thread() {
-         public void run() {
-            System.out.println("Closing MongoDB connections through shutdown hook");
-            if (hostMongoS != null) {
-               hostMongoS.close();
-            }
-            if (timeClient != null) {
-               timeClient.close();
-            }
-            if (shardSetClients != null) {
-               for (MongoClient repClient : shardSetClients.values()) {
-                  repClient.close();
-               }
-            }
-            try {
-               mainThread.join();
-            }
-            catch (InterruptedException e) {
-               System.out.println("---------------- Unable to join main thread, attempting to shutdown MongoDB connections gracefully. --------------");
-               if (hostMongoS != null) hostMongoS.close();
-               if (timeClient != null) timeClient.close();
-               if (shardSetClients != null) {
-                  for (MongoClient repClient : shardSetClients.values()) {
-                     repClient.close();
-                  }
-               }
-               throw new RuntimeException(e);
-            }
-         }
-      });
-   }
+	public static void addShutdownHookToMainThread() {
+		final Thread mainThread = Thread.currentThread();
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				System.out
+						.println("Closing MongoDB connections through shutdown hook");
+				if (hostMongoS != null) {
+					hostMongoS.close();
+				}
+				if (timeClient != null) {
+					timeClient.close();
+				}
+				if (shardSetClients != null) {
+					for (MongoClient repClient : shardSetClients.values()) {
+						repClient.close();
+					}
+				}
+				try {
+					mainThread.join();
+				} catch (InterruptedException e) {
+					System.out
+							.println("---------------- Unable to join main thread, attempting to shutdown MongoDB connections gracefully. --------------");
+					if (hostMongoS != null)
+						hostMongoS.close();
+					if (timeClient != null)
+						timeClient.close();
+					if (shardSetClients != null) {
+						for (MongoClient repClient : shardSetClients.values()) {
+							repClient.close();
+						}
+					}
+					throw new RuntimeException(e);
+				}
+			}
+		});
+	}
 
 }
