d65b99a6b672fd8de9e4e29cdd69144a1f84bafb
Jai Hirsch <jai.hirsch@gmail.com>
2014-12-02 23:25:44 -0600

    fixed bug where default tail type was not being added correctly when no tail type argument was passed

--- mongo-tail/src/main/java/org/mongo/tail/TailTypeInjector.java
+++ mongo-tail/src/main/java/org/mongo/tail/TailTypeInjector.java
@@ -7,30 +7,28 @@ import org.mongo.tail.types.NoopTailType;
 
 public class TailTypeInjector {
 
-   public List<TailType> getTailTypeFromArgs(String... types) {
-      List<TailType> tailers = new LinkedList<TailType>();
-      TailType tailType = null;
-      for (String type : types) {
-         if ("".equals(type)) {
-            System.out.println("------------- WARNING -----------------");
-            System.out
-                  .println("---- No Tail Type passed as argument, using default NoOpTailType ----");
-            tailType = new NoopTailType();
-         } else {
-            Class<?> clazz;
-            try {
-               clazz = Class.forName("org.mongo.tail.types." + type);
-               tailType = (TailType) clazz.newInstance();
-               tailers.add(tailType);
-            } catch (ClassNotFoundException | InstantiationException
-                  | IllegalAccessException e) {
-               e.printStackTrace();
-               throw new RuntimeException(
-                     "Unknown tail type passed to TailTypeInjector.  You must pass the name of the TailType class.");
+    public List<TailType> getTailTypeFromArgs(String... types) {
+        List<TailType> tailers = new LinkedList<TailType>();
+        TailType tailType = null;
+        for (String type : types) {
+            if ("".equals(type)) {
+                System.out.println("------------- WARNING -----------------");
+                System.out.println("---- No Tail Type passed as argument, using default NoOpTailType ----");
+                tailers.add(new NoopTailType());
+            } else {
+                Class<?> clazz;
+                try {
+                    clazz = Class.forName("org.mongo.tail.types." + type);
+                    tailType = (TailType) clazz.newInstance();
+                    tailers.add(tailType);
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException(
+                            "Unknown tail type passed to TailTypeInjector.  You must pass the name of the TailType class.");
+                }
             }
-         }
-      }
-      return tailers;
-   }
+        }
+        return tailers;
+    }
 
 }
